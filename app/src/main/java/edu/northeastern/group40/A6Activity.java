218b05714489739ba6315871cf871b00caef40f3
Tingting-Jiang <jtting@126.com>
2023-02-15 09:17:51 -0800

    Tingting: add main activity button and A6 activity

---
+++ app/src/main/java/edu/northeastern/group40/A6Activity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.group40;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class A6Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a6);
+    }
+}
--- app/src/main/java/edu/northeastern/group40/MainActivity.java
+++ app/src/main/java/edu/northeastern/group40/MainActivity.java
@@ -2,7 +2,9 @@ package edu.northeastern.group40;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onA6Activity(View view) {
+        openNewActivity(A6Activity.class);
+    }
+
+    private void openNewActivity(Class newActivity) {
+        Intent intent = new Intent (MainActivity.this, newActivity);
+        startActivity(intent);
     }
 }
