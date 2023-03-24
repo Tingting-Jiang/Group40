f6530f0540cef6b3211edb8ed11f71a96138a810
ruihan <ding.ruih@northeastern.edu>
2023-03-24 00:06:53 -0700

    add the search screen UI

---
+++ app/src/main/java/edu/northeastern/group40/Project/SearchActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.group40.Project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.group40.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
