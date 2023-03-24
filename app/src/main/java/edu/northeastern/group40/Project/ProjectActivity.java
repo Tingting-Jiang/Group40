1991651cdc8d6926a7f0191fb104dd601c34bbaf
ruihan <ding.ruih@northeastern.edu>
2023-03-24 16:58:15 -0700

    select time func and edit some strings

--- app/src/main/java/edu/northeastern/group40/Project/ProjectActivity.java
+++ app/src/main/java/edu/northeastern/group40/Project/ProjectActivity.java
@@ -2,7 +2,9 @@ package edu.northeastern.group40.Project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import edu.northeastern.group40.R;
 
@@ -12,5 +14,10 @@ public class ProjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
+    }
+
+    public void openSearchActivity(View view) {
+        Intent intent = new Intent (ProjectActivity.this, SearchActivity.class);
+        startActivity(intent);
     }
 }
