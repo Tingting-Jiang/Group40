1fdeee91d45430ada63474999e09ad7eda02b075
Zhehao <x97589125@gmail.com>
2023-03-28 15:13:20 -0700

    Add title price and image

--- app/src/main/java/edu/northeastern/group40/Project/Models/VehicleInterface.java
+++ app/src/main/java/edu/northeastern/group40/Project/Models/VehicleInterface.java
@@ -1,5 +1,7 @@
 package edu.northeastern.group40.Project.Models;
 
+import android.graphics.Bitmap;
+
 import com.google.android.libraries.places.api.model.Place;
 
 public interface VehicleInterface {
@@ -11,4 +13,9 @@ public interface VehicleInterface {
     Mileage getMileage();
     int getCapacity();
     Place getPlace();
+    Bitmap getImage();
+    String getTitle();
+    int getReviewTotalNumber();
+    String getReviewResult();
+    int getRentPrice();
 }
