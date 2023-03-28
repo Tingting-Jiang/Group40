ead05cb84a310a43b8473a365e4c3e7bc6ed2f1c
Tingting-Jiang <jtting@126.com>
2023-03-28 15:16:45 -0700

    Add DB to carList activity

--- app/src/main/java/edu/northeastern/group40/Project/CarListActivity.java
+++ app/src/main/java/edu/northeastern/group40/Project/CarListActivity.java
@@ -1,21 +1,29 @@
 package edu.northeastern.group40.Project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.google.android.material.chip.Chip;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.northeastern.group40.A8.Models.User;
 import edu.northeastern.group40.Project.Models.Car;
 import edu.northeastern.group40.Project.Models.SelectListener;
 import edu.northeastern.group40.Project.RecyclerView.CarListAdapter;
@@ -24,13 +32,16 @@ import edu.northeastern.group40.R;
 public class CarListActivity extends AppCompatActivity implements SelectListener {
     private CarListAdapter carListAdapter;
     private static final String TAG = "CarListActivity";
-    private final List<Car> carList = new ArrayList<>();
+    private final List<Car> vehicleList = new ArrayList<>();
     private String rentCarType;
     private String rentDate;
     private boolean displayPriceLowToHigh = false;
     private Chip rentTypeChip, rentDateChip, rentPriceChip;
     private static final String PRICE_LOW_TO_HIGH = "Low to High";
     private static final String PRICE_HIGH_TO_LOW = "High to Low";
+    private final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference usersDB;
+    private DatabaseReference vehicleDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,10 @@ public class CarListActivity extends AppCompatActivity implements SelectListener
 
     private void init() {
         // Todo: get  the filter items
+        usersDB = mDatabase.child("Users");
+        vehicleDB = mDatabase.child("Vehicles");
+        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert currUser != null;
 
         this.rentTypeChip = findViewById(R.id.car_type_chip);
         this.rentDateChip = findViewById(R.id.rent_time_chip);
@@ -76,20 +91,43 @@ public class CarListActivity extends AppCompatActivity implements SelectListener
 
 
         //TODO: GET DATA FROM DB
-        if (carList.size() == 0) {
+
+        fetchDataFromDB();
+        if (vehicleList.size() == 0) {
             for (int i = 0; i < 5; i++) {
-                carList.add(new Car("2022 Lexus", "3000", "5.0", String.valueOf(i), "9.8", "123","88"));
+                vehicleList.add(new Car("2022 Lexus", "3000", "5.0", String.valueOf(i), "9.8", "123","88"));
             }
         }
 
 
     }
 
+    private void fetchDataFromDB() {
+        vehicleDB.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                vehicleList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Car currCar = dataSnapshot.getValue(Car.class);
+                    assert currCar != null;
+                    // todo: filter cars that meet requirement
+
+                }
+                carListAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
     private void initRecyclerView() {
         RecyclerView carRecView = findViewById(R.id.car_list_recView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         carRecView.setLayoutManager(layoutManager);
-        carListAdapter = new CarListAdapter(CarListActivity.this, carList, this);
+        carListAdapter = new CarListAdapter(CarListActivity.this, vehicleList, this);
         carRecView.setAdapter(carListAdapter);
 
 
