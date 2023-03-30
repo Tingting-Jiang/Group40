de7c9a75b467f48c1bba55866b2b005c8ae55cd2
cychan <87886012+cychan2047@users.noreply.github.com>
2023-03-29 17:40:14 -0700

    Add order class Add enum class review

--- app/src/main/java/edu/northeastern/group40/Project/Models/Order.java
+++ app/src/main/java/edu/northeastern/group40/Project/Models/Order.java
@@ -1,5 +1,52 @@
 package edu.northeastern.group40.Project.Models;
 
 public class Order {
-    // TODO: the order
+    String orderId;
+
+    Vehicle orderedVehicle;
+
+    AvailableDate orderDate;
+
+    int orderPriceTotal;
+
+    Review review;
+
+    public Order(String orderId, Vehicle orderedVehicle, AvailableDate orderDate, int orderPriceTotal) {
+        this.orderId = orderId;
+        this.orderedVehicle = orderedVehicle;
+        this.orderDate = orderDate;
+        this.orderPriceTotal = orderPriceTotal;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public Vehicle getOrderedVehicle() {
+        return orderedVehicle;
+    }
+
+    public void setOrderedVehicle(Vehicle orderedVehicle) {
+        this.orderedVehicle = orderedVehicle;
+    }
+
+    public AvailableDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(AvailableDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getOrderPriceTotal() {
+        return orderPriceTotal;
+    }
+
+    public void setOrderPriceTotal(int orderPriceTotal) {
+        this.orderPriceTotal = orderPriceTotal;
+    }
 }
